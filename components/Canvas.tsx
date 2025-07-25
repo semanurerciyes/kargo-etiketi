fd82986a04a03171301baac99c6f1c369c040e8b
semanurerciyes <semanurerciyes2@gmail.com>
2025-07-25 13:51:38 +0300

    Update Canvas.tsx

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import React from "react";
 
 const Canvas = () => {
-  return <div>Canvas alanı</div>;
+  return (
+    <div>
+      {/* Buraya canvas içeriğini ekle */}
+      <h1>Canvas Bileşeni</h1>
+    </div>
+  );
 };
 
 export default Canvas;
